4618599a52cf0d75cd6e1130e310b556d19a3e26
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2024-07-12 22:08:26 +0700

    refactor : added logic into splash view when the view appear then do 3 seconds change to the onboarding page

--- Medcare/SplashView.swift
+++ Medcare/SplashView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var isActive = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+            Text("Hello world")
+        }else {
+            ZStack {
+                Color("SplashColor")
+                    .ignoresSafeArea()
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:183, height:166.58)
+                
+            }.onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now()+3, execute: {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                })
+            }
+        }
+       
     }
 }
 
