b1be7166e39a534f3a1b404d97cbd55ab34e591a
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2024-07-12 21:51:01 +0700

    refactor: change the content view to be splash view

---
+++ Medcare/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Medcare
+//
+//  Created by Abdul Azis  Al Ayubbi on 12/07/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
