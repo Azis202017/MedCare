b56410c46b7bd2ff05870b3bec49d9cbbe3ad76f
Abdul Azis Al Ayubbi <azisa6980@gmail.com>
2024-07-12 18:08:36 +0700

    Initial Commit

---
+++ Medcare/MedcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedcareApp.swift
+//  Medcare
+//
+//  Created by Abdul Azis  Al Ayubbi on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MedcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedcareUITests/MedcareUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MedcareUITestsLaunchTests.swift
+//  MedcareUITests
+//
+//  Created by Abdul Azis  Al Ayubbi on 12/07/24.
+//
+
+import XCTest
+
+final class MedcareUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
